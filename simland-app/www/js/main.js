1a6ea99029f182dedf8117dc82662e8a258c8a52
zhijun0808 <zhijun0808@gmail.com>
2015-06-10 13:10:43 +0800

    ok

--- simland-app/www/js/main.js
+++ simland-app/www/js/main.js
@@ -14,8 +14,23 @@ var init = function() {
 	
 	$("#searchCategoryBtn").click(searchCategoryBtn);
 	
+	//购物车商品全选按钮
+	$("#carCheckAll").click(carCheckAll);
+	
+	
 };
 $(document).ready(init);
+
+function carCheckAll(){
+	
+	if($(this).is(":checked")){
+		$("input[name='carCheck']").attr("checked", true);  
+	}else{
+	    $("input[name='carCheck']").attr("checked", false);  
+	}
+	
+
+}
 
 
 function searchCategoryFocus(){
