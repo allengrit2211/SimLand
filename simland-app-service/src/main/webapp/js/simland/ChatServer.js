08a99785096e4680335c4114840a7854dd5efe77
zhijun0808 <zhijun0808@aliyun.com>
2015-08-30 18:43:11 +0800

    客服系统

---
+++ simland-app-service/src/main/webapp/js/simland/ChatServer.js
@@ -0,0 +1,32 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.get('/', function(req, res){
+	res.send('<h1>Welcome Realtime Server</h1>');
+});
+
+
+
+var users = {};
+
+io.on('connection', function(socket){
+	
+	console.log('a user connected');
+	
+	socket.on('private message', function (sign,msg) {
+		console.log('private message');
+		io.sockets.in('group'+sign).emit('event_name', msg);
+	});
+	
+	
+  socket.on('new user',function(sign){
+  	console.log('group'+sign);
+		socket.join('group'+sign);
+  });	
+
+});
+
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
